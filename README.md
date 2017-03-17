8aba0ba146cdb254630626173ea8756fc8d39846
OGKevin <kevinhellemun@icloud.com>
2017-03-17 11:43:26 +0100

    all done

---
+++ README.md
@@ -0,0 +1,16 @@
+## FCC Calculator
+
+As described on [FCC](https://www.freecodecamp.com/challenges/build-a-javascript-calculator) website the user should be able to:
+
+* Add, subtract, multiply and divide two numbers.
+* Clear the input field with a clear button.
+* Keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
+
+
+## Link
+ (http://ogkevincalculator.surge.sh)
+ 
+## Disclaimer
+HTML and CSS of this porject can be found here: (https://codepen.io/ckm100/pen/dPLyjZ)
+
+JS of this project is made by OGKevin
--- master.js
+++ master.js
@@ -6,79 +6,79 @@ var values = {
     i = 1,
     count = -1,
     reset = false;
-$(function() {
+$(function() { // NOTE: the click actions
     $(".num").click(function(event) {
-      if (reset) {
-        del();
-      }
+        if (reset) {
+            del();
+        }
         values[i].push($(this).val());
         $("#result").html(values[i].join(""))
-        console.log($(this).val() + "-->" + values[i].join("") + " values[" + i + "]");
     });
     $(".period").click(function(event) {
-      values[i].push($(this).val());
+        values[i].push($(this).val());
     });
     $(".operator").click(function(event) {
+        if (reset) {
+            reset = false;
+
+        }
         count++;
-        console.log("count = " + count);
         oper.push($(this).val());
-        console.log(oper);
         change(oper.length, count);
     });
     $("#resultSign").click(function(event) {
-        if (Array.isArray(values[1])) {
+        if (Array.isArray(values[1]) && values[2][0] !== undefined) {
             cal(values[1].join(""), values[2].join(""), oper[count]);
-        } else {
+            reset = true;
+        } else if (values[2][0] !== undefined) {
             cal(values[1], values[2].join(""), oper[count]);
+            reset = true;
         }
-        reset = true;
     });
     $("#delete").click(function(event) {
         del();
     })
 })
 
-function cal(a, b, operator) {
+function cal(a, b, operator) { // NOTE: does the calculations
     var results = 0;
     switch (operator) {
         case "*":
             results = a * b;
-            console.log("case *" + " " + a + " " + b);
             break;
         case "/":
             results = a / b;
-            console.log("case /" + " " + a + " " + b);
             break;
         case "-":
             results = a - b;
-            console.log("Case -" + " " + a + " " + b);
             break;
         case "+":
             results = Number(a) + Number(b);
-            console.log("case +" + " " + a + " " + b);
             break;
         case "%":
             results = a % b;
-            console.log("case %" + " " + a + " " + b);
             break;
         default:
-            console.log("no case");
     }
-    $("#result").html(results);
-    console.log(results);
+
+    if (results % 1 !== 0) { // NOTE: if float or not
+        $("#result").html(results.toFixed(2));
+    } else {
+        $("#result").html(results);
+
+    }
     return Number(results)
 }
 
 function change(length) {
     if (length === 1) { // NOTE: switches to values[2]
         i = 2;
-    } else if (length > 1) { // NOTE: handels the cahining
+    } else if (length > 1) { // NOTE: handels the chaining
         if (Array.isArray(values[1])) {
             values[1] = cal(values[1].join(""), values[2].join(""), oper[count - 1]);
         } else {
             values[1] = cal(values[1], values[2].join(""), oper[count - 1]);
         }
-        // console.log(values[1]);
         values[i].splice(0, values[i].length); // NOTE clears values[2]
     }
 }
@@ -93,5 +93,4 @@ function del() {
     count = -1;
     reset = false;
     $("#result").html("");
-    console.log("cleared");
 }
